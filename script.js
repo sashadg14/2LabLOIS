0a6b4c86e50ce132bd61c0c4ad73da7dc58648a3
sashadg14 <mamayko.1998@mail.ru>
2018-02-13 16:07:13 +0300

    pre final 2

--- script.js
+++ script.js
@@ -135,14 +135,6 @@ function isLetter(s) {
     else return false;
 }
 
-function addInSetIfVariable(s) {
-    if (s >= 'A' && s <= 'K') {
-        addInSet(s);
-        return true;
-    }
-    else return false;
-}
-
 function isVariable(s) {
     if (s >= 'A' && s <= 'Z') {
         return true;
@@ -165,6 +157,22 @@ function priority(op) {
         default:
             return -1;
     }
+}
+
+function replase_operations_res(str){
+    var regexp = /([A-Z,1,0])(->|&|~|\|)([A-Z,1,0])/g;
+    var ab = str.replace(regexp,"A");
+    if(ab!=null||ab!=undefined)
+        return ab;
+    else return str;
+}
+
+function replase_impl(str){
+    var regexp = /->/g;
+    var ab = str.replace(regexp,"-");
+    if(ab!=null||ab!=undefined)
+        return ab;
+    else return str;
 }
 
 function calculate(s){
@@ -213,7 +221,8 @@ function initCalculation(s){
             values.shift();
         }
       //  alert(values+" "+initVariablesInExpression(s));
-        if(calculate(initVariablesInExpression(s))==0) {
+
+        if(calculate(replase_impl(initVariablesInExpression(s)))==0) {
             tr = false;
             break;
         }
